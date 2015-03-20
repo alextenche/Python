b5aa04980e32e4e0d3315ad8a89488f4e8345b7f
alexTenche <alex.tenche@gmail.com>
2015-03-20 16:50:20 +0200

    working on pong

---
+++ psd_ai.py
@@ -0,0 +1,19 @@
+# AI  de PSD-ist
+
+def haos():
+    read_DCNEWS()
+    watch_RTV()
+    watch_Antena3()
+    listen_Ciutacu()
+    
+    
+def ce_sa_mai_furam():
+    if(mai e ceva de furat):
+        furam_frate()
+    else:
+        hai_ma_ca_mai_tre_sa_fie_ceva()
+        
+    
+while(1):
+    haos()
+    ce_sa_mai_furam(
