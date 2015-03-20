3adba53ab683cc04274ede56d983b4327b0efdf5
alexTenche <alex.tenche@gmail.com>
2015-03-20 08:50:33 +0200

    lists

---
+++ lists.py
@@ -0,0 +1,26 @@
+# list fun
+
+# create
+mt_list = []
+print mt_list
+
+l1 = [1, 2, 3, 5, -11, -23]
+print l1
+
+l2 = ['ana', 'are', 'mere', 'si', 'pere']
+print l2
+
+l3 = [[3, 4, 5], ['a', 'b', 'c'], []]
+print l3
+
+# access
+print len(mt_list), len(l1), len(l3)
+print "first element ", l1[0]
+print "last element ", l1[-1]
+print l3[1]
+l4 = l2[1:3]
+print l4
+
+#update
+l2[0] = 'chesse'
+print l2
