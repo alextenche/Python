2773b74a27cd7ad6489d48e0b554b0a96732a6bb
Alexandru Tenche <alex.tenche@gmail.com>
2017-04-15 00:46:13 +0300

    added remove item from list

--- shoppingList.py
+++ shoppingList.py
@@ -1,39 +1,86 @@
+import os
 
 shoppingList = []
 
+
+def clearScreen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def showHelp():
+    clearScreen()
     print("what should we pick up at the store")
     print("""
 "enter 'DONE' to stop"
 "enter 'HELP' to show help"
 "enter 'SHOW' to display current list"
+"enter 'REMOVE' to delete an item from the list"
 """)
 
+
 def showList():
+    clearScreen()
     print("here it's your shopping list:")
+
+    index = 1
     for item in shoppingList:
-        print(item)
+        print("{}. {}".format(index, item))
+        index += 1
+    print("-"*10)
+
 
 def addToList(item):
-    shoppingList.append(item)
-    print("added {}, list now has {} items".format(newItem, len(shoppingList)))
-    
+    showList()
+    if len(shoppingList):
+        position = input("where should I add {}\n"
+                         "press enter to add to the end of the list"
+                         "> ".format(item))
+    else:
+        position = 0
 
-showHelp();
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+
+    if position is not None:
+        shoppingList.insert(position - 1, item)
+    else:
+        shoppingList.append(newItem)
+
+    showList()
+
+def removeFromList():
+    showList()
+
+    whatToRemove = input("what yould you like to remove ?\n>")
+
+    try:
+        shoppingList.remove(whatToRemove)
+    except ValueError:
+        pass
+
+    showList()
+
+
+
+showHelp()
 
 while True:
     newItem = input("> ")
 
-    if newItem == 'DONE':
-        break;
-    elif newItem == 'HELP':
+    if newItem.upper() == 'DONE' or newItem.upper() == 'QUIT':
+        break
+    elif newItem.upper() == 'HELP':
         showHelp()
         continue
-    elif newItem == 'SHOW':
+    elif newItem.upper() == 'SHOW':
         showList()
         continue
-
-    addToList(newItem)
-
+    elif newItem.upper() == 'REMOVE':
+        removeFromList()
+        continue
+    else:
+        addToList(newItem)
 
 showList()
