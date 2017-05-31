7522c3fe35283acec30d1042c44d23fdcf7fbc5d
Alexandru Tenche <alex.tenche@gmail.com>
2017-05-31 03:23:56 +0300

    collections

---
+++ collectionsTests.py
@@ -0,0 +1,13 @@
+courses_minutes = {'Test1': 12, 'Test2': 15, 'Test3': 30}
+
+for course in courses_minutes:
+    print(course)
+
+for key in courses_minutes.keys():
+    print(key)
+
+for value in courses_minutes.values():
+    print(value)
+
+for item in courses_minutes.items():
+    print(item)
