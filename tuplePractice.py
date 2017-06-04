1789bb7110666e07e0a845f358707f48e2a8b279
Alexandru Tenche <alex.tenche@gmail.com>
2017-06-04 14:44:05 +0300

    more tuple practice

--- tuplePractice.py
+++ tuplePractice.py
@@ -19,3 +19,24 @@ print(multiply(1, 5, 'alex'))
 
 # enumerate
 print(list(enumerate("alexone")))
+
+
+# different cases for a string
+def stringcases(text):
+    return text.lower(), text.upper(), text.title(), text[::-1]
+
+print(stringcases("vonavi"))
+
+
+# combo([1, 2, 3], 'abc')
+# Output:
+# [(1, 'a'), (2, 'b'), (3, 'c')]
+def combo(iter1, iter2):
+    result = []
+    for index, item in enumerate(iter1):
+        pair = iter1[index], iter2[index]
+        result.append(pair)
+    return result
+
+print(combo([1, 2, 3], 'abc'))
+
