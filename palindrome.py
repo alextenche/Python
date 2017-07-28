ea10bbcaef5a3ab0529d02ea8ba41379c33ee968
Alexandru Tenche <alex.tenche@gmail.com>
2017-07-28 18:14:46 +0200

    finished palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,34 @@
 def is_palindrome_v1(s):
-    return True
+    return reverse(s) == s
+
+
+def is_palindrome_v2(s):
+    n = len(s)
+    return s[:n // 2] == reverse(s[n - n // 2:])
+
+
+def is_palindrome_v3(s):
+    i = 0
+    j = len(s) - 1
+
+    while i < j and s[i] == s[j]:
+        i += 1
+        j -= 1
+
+    return j <= i
 
 
 def reverse(s):
     rev = ''
 
     for ch in s:
-        rev += ch
+        rev = ch + rev
 
     return rev
 
 
-print(reverse('hello'))
+# print(reverse('hello'))
+
+print(is_palindrome_v3('noon'))
+print(is_palindrome_v3('racecar'))
+print(is_palindrome_v3('dented'))
