49f346c55909cc6da1995e3d5927e6bb1779825c
Alexandru Tenche <alex.tenche@gmail.com>
2017-08-12 12:06:55 +0200

    fix and added doctest

--- coursera/linear_search.py
+++ coursera/linear_search.py
@@ -1,21 +1,27 @@
 def linear_search(L, v):
-    ''' (list, object) -> int
+    """ (list, object) -> int
 
     return the first occurrence of v in L, or -1 if not found
 
-    >>>linear_searc([1, 2, 3, 4], 1)
+    >>> linear_search([1, 2, 3, 4], 1)
     0
-    >>>linear_searc([1, 2, 3, 4], 3)
+    >>> linear_search([1, 2, 3, 4], 3)
     2
-    >>>linear_searc([1, 2, 3, 4], 8)
+    >>> linear_search([1, 2, 3, 4], 8)
     -1
-    '''
+    """
 
     i = 0
-    while i != len(L) | v != L[i]:
-        i += 1
+    while i != len(L) and v != L[i]:
+        i = i + 1
 
     if i == len(L):
         return -1
     else:
         return i
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
